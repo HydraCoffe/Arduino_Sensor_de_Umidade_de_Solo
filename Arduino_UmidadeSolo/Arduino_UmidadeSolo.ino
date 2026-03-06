9e33f5f6e101166b61a09bf3b266a44433d57c48
Matheus-GoncalvesSptech <matheus.sgoncalves@sptech.school>
2026-03-06 13:43:59 -0300

    atualizacao codigo arduino 1

---
+++ Arduino_UmidadeSolo/Arduino_UmidadeSolo.ino
@@ -0,0 +1,38 @@
+const int ValorAr = 437;    // leitura no ar valor de calibração
+const int ValorAgua = 187;  //leitura na água valor de calibração
+
+int valorUmidadeSolo = 0;
+float porcentagemUmidade = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  valorUmidadeSolo = analogRead(A4);
+
+  int faixa = ValorAr - ValorAgua;
+
+  int distancia = ValorAr - valorUmidadeSolo;
+
+  porcentagemUmidade = (float)distancia / faixa * 100.0;
+
+  if (porcentagemUmidade < 0) porcentagemUmidade = 0;
+  if (porcentagemUmidade > 100) porcentagemUmidade = 100;
+
+  Serial.print("Leitura bruta: ");
+  Serial.print(valorUmidadeSolo);
+  Serial.print(" | Umidade: "); //codigo para ver o Serial Monitor
+  Serial.print(porcentagemUmidade);
+  Serial.println("%");
+
+  delay(1000);
+
+  /*Serial.println(porcentagemUmidade); //codigo para visualizar o grafico(Serial Plotter)
+
+  delay(700);*/
+
+}
+
+
+
