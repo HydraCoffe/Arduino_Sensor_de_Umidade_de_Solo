499ee89cf059d52f82b9e4ed9cc5ac087bf776c0
Matheus-GoncalvesSptech <matheus.sgoncalves@sptech.school>
2026-03-06 12:50:26 -0300

    atualizacao do codigo arduino

--- Arduino_UmidadeSolo.ino
+++ Arduino_UmidadeSolo.ino
@@ -1,5 +1,5 @@
-const int ValorAr = 550;    // leitura no ar valor de calibração
-const int ValorAgua = 230;  //leitura na água valor de calibração
+const int ValorAr = 437;    // leitura no ar valor de calibração
+const int ValorAgua = 187;  //leitura na água valor de calibração
 
 int valorUmidadeSolo = 0;
 float porcentagemUmidade = 0;
@@ -9,13 +9,13 @@ void setup() {
 }
 
 void loop() {
-  valorUmidadeSolo = analogRead(A2);
+  valorUmidadeSolo = analogRead(A4);
 
   int faixa = ValorAr - ValorAgua;
 
   int distancia = ValorAr - valorUmidadeSolo;
 
-  porcentagemUmidade = 100 - ((float)distancia / faixa * 100.0);
+  porcentagemUmidade = (float)distancia / faixa * 100.0;
 
   if (porcentagemUmidade < 0) porcentagemUmidade = 0;
   if (porcentagemUmidade > 100) porcentagemUmidade = 100;
@@ -28,3 +28,6 @@ void loop() {
 
   delay(1000);
 }
+
+
+
